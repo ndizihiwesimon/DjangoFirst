f011df19f41d4d9d39c9a26ed4b14552a1dbc70a
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-20 10:16:15 +0200

    Trainee model

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Trainee(models.Model):
+    full_name = models.CharField(max_length=100)
+    dob = models.DateField()
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.full_name
